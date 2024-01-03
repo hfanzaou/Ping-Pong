9225927b63d0e7ea43dfe21fa92e19f9728ae9c2
rizqyrahhal <rizqyrahhal8@gmail.com>
2024-01-03 02:16:43 +0100

    upload avatar don! rest handel error case

--- frontend/src/pages/private/Settings/FditeProfile/UpdateAvatar.tsx
+++ frontend/src/pages/private/Settings/FditeProfile/UpdateAvatar.tsx
@@ -17,56 +17,51 @@ const file2Base64 = (file: File): Promise<string> => {
 
 function ChangeAvatar({settAvatar, avatar} : {settAvatar: Function, avatar: string})
 {
+    const [userimage, setUserImage] = useState<string>(avatar);
+    const [save, setSave] = useState<boolean>(true);
+    const [uploaded, setUploaded] = useState<FormData | undefined>();
+    const fileRef = createRef<HTMLInputElement>();
 
-//   const [setAvatar, setSetAvatar] = useState<boolean>(false);
-  const [userimage, setUserImage] = useState<string>(avatar);
-//   const [image, setImage] = useState<string| undefined>(avatar);
-  const [save, setSave] = useState<boolean>(true);
+    const onFileInputChange: React.ChangeEventHandler<HTMLInputElement> = async (e) => {
+        e.preventDefault();
+        const file = e.target?.files?.[0];
+        const formData = new FormData();
+        formData.append('File', file as Blob);
 
-const [uploaded, setUploaded] = useState<FormData | undefined>();
-  const fileRef = createRef<HTMLInputElement>();
+        setUploaded(formData);
+        if (file) {
+            file2Base64(file).then((base64) => {
+                setUserImage(base64);
+            })
+        }
 
-  const onFileInputChange: React.ChangeEventHandler<HTMLInputElement> = async (e) => {
-    e.preventDefault();
-    const file = e.target?.files?.[0];
-    const formData = new FormData();
-    formData.append('File', file as Blob);
-
-    setUploaded(formData);
-    if (file) {
-      file2Base64(file).then((base64) => {
-        setUserImage(base64);
-      })};
-
-    setSave(false);
+        setSave(false);
 
         // Reset the value of the file input
         if (e.target) {
             e.target.value = '';
-          }
-  };
+        }
+    }
 
-  const handleRest = () => {
+    const handleRest = () => {
         setSave(true);
         setUserImage(avatar);
-  };
+    }
 
-  const handleSaveAvatar = async () => {
+    const handleSaveAvatar = async () => {
+        await axios.post('http://localhost:3001/settings/avatar',  uploaded)
+        .then(res => {
+            console.log(res.data);
+            setSave(true);
+            settAvatar(userimage);
+        })
+        .catch(err => {
+            console.error("Error in send profile info: ", err);
+        })
+    }
 
-    await axios.post('http://localhost:3001/settings/avatar',  uploaded)
-    .then(res => {
-      console.log(res.data);
-      setSave(true);
-      settAvatar(userimage);
-    })
-    .catch(err => {
-      console.error("Error in send profile info: ", err);
-    }) 
-  };        
-
-  return (
-      <div className='grid  place-items-center'>
-
+    return (
+        <div className='grid  place-items-center'>
             <input
                 type="file"
                 style={{display: 'none'}}
@@ -74,24 +69,23 @@ const [uploaded, setUploaded] = useState<FormData | undefined>();
                 onChange={onFileInputChange}
                 accept="image/png,image/jpeg,image/gif"
               />
-<div dir="rtl" className="relative h-32 w-32"  onClick={() => fileRef.current?.click()}>
-        <button   type="button" className="relative inline-flex items-center justify-center rounded-full p-1 text-gray-600 hover:bg-gray-900 hover:text-white">
-          
-          <Avatar size='xl' src={!userimage ? avatar : userimage} />
-          <div className="absolute h-14 w-14 top-1 start-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
-              <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
-            </svg>
-          </div>
-        </button>
+            <div dir="rtl" className="relative h-32 w-32"  onClick={() => fileRef.current?.click()}>
+                <button   type="button" className="relative inline-flex items-center justify-center rounded-full p-1 text-gray-600 hover:bg-gray-900 hover:text-white">
+                    <Avatar size='xl' src={!userimage ? avatar : userimage} />
+                    <div className="absolute h-14 w-14 top-1 start-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                        </svg>
+                    </div>
+                </button>
+            </div>
+            {!save && 
+                <div className="mb-8">
+                    <Button color="gray" radius='xl' onClick={handleSaveAvatar}>Set new Avatar</Button>
+                    <Button color="gray"  ml={8} radius='xl' onClick={handleRest} >Discard</Button>
+                </div>}
         </div>
-                    {!save && 
-                        <div>
-                        <Button color="gray" radius='xl' onClick={handleSaveAvatar}>Set new Avatar</Button>
-                        <Button color="gray" radius='xl' onClick={handleRest} >Cancel</Button>
-                        </div>}
-      </div>
     );
 }
                     
