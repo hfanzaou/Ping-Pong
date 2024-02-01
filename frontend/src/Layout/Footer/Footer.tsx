92e6193d1a2eaa75939d1bb39252ed002f0adfbe
rizqyrahhal <rizqyrahhal8@gmail.com>
2024-02-01 01:31:51 +0100

    color

--- frontend/src/Layout/Footer/Footer.tsx
+++ frontend/src/Layout/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Avatar, Card } from "@mantine/core";
+import { Avatar, HoverCard, HoverCardDropdown, HoverCardTarget, Text } from "@mantine/core";
 
 function Footer() {
     return (
@@ -9,10 +9,38 @@ function Footer() {
                 <p className="text-sm">made with ♡ by</p>
             </div>
             <div className="flex items-center space-x-5">
-                <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/a20a415122c250b00685308ae3909e21/hfanzaou.jpg'/>
-                <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/e309a3cba8f23ed14b44f453ff827085/ajana.jpg'/>
-                <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/4a7006c8b7336b000e1cfaab5db88538/ebensalt.jpg'/>
-                <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/0aab98ac802b6cd5ef94b9c3ea1b0699/rarahhal.jpg'/>
+                <HoverCard>
+                    <HoverCardTarget>
+                        <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/a20a415122c250b00685308ae3909e21/hfanzaou.jpg'/>
+                    </HoverCardTarget>
+                    <HoverCardDropdown bg='dark'>
+                        <Text c='cyan'>fanzaou hatim</Text>
+                    </HoverCardDropdown>
+                </HoverCard>
+                <HoverCard>
+                    <HoverCardTarget>
+                        <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/e309a3cba8f23ed14b44f453ff827085/ajana.jpg'/>
+                    </HoverCardTarget>
+                    <HoverCardDropdown bg='dark'>
+                        <Text c='cyan'>ajana ayoub</Text>
+                    </HoverCardDropdown>
+                </HoverCard>
+                <HoverCard>
+                    <HoverCardTarget>
+                        <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/4a7006c8b7336b000e1cfaab5db88538/ebensalt.jpg'/>
+                    </HoverCardTarget>
+                    <HoverCardDropdown bg='dark'>
+                        <Text c='cyan'>ebensalt mahdi</Text>
+                    </HoverCardDropdown>
+                </HoverCard>
+                <HoverCard>
+                    <HoverCardTarget>
+                        <Avatar size='8vh' radius='md' src='https://cdn.intra.42.fr/users/0aab98ac802b6cd5ef94b9c3ea1b0699/rarahhal.jpg'/>
+                    </HoverCardTarget>
+                    <HoverCardDropdown bg='dark'>
+                        <Text c='cyan'>Rizqy Rahhal</Text>
+                    </HoverCardDropdown>
+                </HoverCard>
             </div>
         </div>
     );
