9031d3526db3fdfcbf9fbed46dddae090d5e5d4c
rizqyrahhal <rizqyrahhal8@gmail.com>
2024-02-03 08:46:12 +0100

    after break fraze

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,22 +6,7 @@ import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-//    <React.StrictMode>
-    <App />
-    );
-    {/* </React.StrictMode>  */}
-
-// const rootjoj = ReactDOM.createRoot(document.getElementById("rootjoj") as HTMLElement);
-// rootjoj.render(
-// //    <React.StrictMode>
-//     <AppJoj />
-//     );
-//     {/* </React.StrictMode>  */}
-// );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
+    // <React.StrictMode>
+        <App />
+    // {/* </React.StrictMode> */}
+);
--- frontend/src/pages/private/Profile/UsersRelation/friends/Friends.tsx
+++ frontend/src/pages/private/Profile/UsersRelation/friends/Friends.tsx
@@ -103,7 +103,7 @@ function  Frindes({socket, setUrlName}: {socket: Socket, setUrlName: Function}) 
         <div>
             {Object.keys(friends).length ?
                 friends :
-                <Blockquote className='xl'  ta='center' color="white" c='cyan' radius="lg" mt="xl">
+                <Blockquote className='text-xl'  ta='center' color="white" c='cyan' radius="lg" mt="xl">
                     Add Freinds to shows them here
                 </Blockquote>
             }    
